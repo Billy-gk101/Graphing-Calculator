16fff93b5325b0a1a95ba628a812cb9f9d1c3f77
Billy-gk101 <45773518+Billy-gk101@users.noreply.github.com>
2024-03-29 19:08:41 -0600

    updated for geometric class changes
    updated Line_Segment to be a container for shapely.LineString to reduce 'reinventing weheel'
    updated Triangle to be a container for shapely Geometry that defines the triangle to reduce 'reinventing weheel'
    While updating classes, tried to follow PEP 257
    Updated display of Precision to remove trailing 0

--- app.py
+++ app.py
@@ -2,17 +2,22 @@
 # CODE IMPORTS
 # =============================================================================
 import os, sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from functools import partial
+from numbers import Number
+import matplotlib
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 import qdarktheme
 
 __doc__     = "Calculator and Math Helper"
-__version__ = "0.2.3"
+__version__ = "0.0.3"
 
 PTH_APP = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(PTH_APP, 'data'))
 
 # ---- Local addins
 from _global_ import *
+matplotlib.use('Qt5Agg')
 from generic_widgets import QCalcWidget, QTriangle_Solver
 import mainWindow
     
@@ -20,7 +25,7 @@ class MainWindow(QMainWindow, mainWindow.Ui_MainWindow):
     def __init__(self, app:QApplication) -> None:
         super().__init__(None)
         self.setupUi(self)
-        self.setWindowTitle(f'{__doc__} [{__version__}]')
+        self.setWindowTitle("Fun with Geometry")
         self.show()
         self._app  = app
 
